d9572223f75b5fc8628d0a0b01f927470d40970a
Marco <maidh91@gmail.com>
2025-10-20 15:30:33 +1100

    minor clean

--- with-events/bin.js
+++ with-events/bin.js
@@ -46,7 +46,7 @@ async function addWriterInfo (room) {
 /** @type {function(MultiWriterRoom)} */
 async function printAllWriters (room) {
   const users = await room.getUsers()
-  console.log('All writers:')
+  console.log('Writers:')
   users
     .sort((a, b) => new Date(b.at) - new Date(a.at))
     .slice(0, 5)
